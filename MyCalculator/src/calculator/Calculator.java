1852a810cd9e1ec32937489a1e43ae8eed9b2558
Dennis Baans <dennis.baans@gmail.com>
2022-02-02 08:13:56 +0000

    Update MyCalculator/src/calculator/Calculator.java

--- MyCalculator/src/calculator/Calculator.java
+++ MyCalculator/src/calculator/Calculator.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 /**
  * Beschreibe doch hier, wie man das Programm benutzt und welche Funktionen alles ausgeführt werden.
- * test test testä hm? a a aaa more??..dasdf testa
+ * test test testä hm? a a aaa more??..dasdf testa2
  */
 public class Calculator{
 
